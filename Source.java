ca38b781dd8abdbc6f73bc5669656b1d55496452
Parham Saniei <parham.saniei@fiserv.com>
2023-11-03 13:54:39 -0400

    solution

--- Source.java
+++ Source.java
@@ -6,13 +6,23 @@ class Node<T> {
       this.val = val;
       this.next = null;
     }
-  }
-  
-  class Source {
-    public static <T> boolean linkedListFind(Node<T> head, T target) {
-      // todo
-      return false;
+}
+
+class Source {
+    public static <T> boolean linkedListFind(Node<T> head, T target) { // checks if a target value exists in linked list
+        Node<T> current = head; 
+        while (current != null) {
+            if (current.val.equals(target)) {
+                return true; 
+            }
+            current = current.next;
+        }
+        return false; 
+        // starts from head and traverses until it finds target val or reaches end of list
+        
     }
+
+    // test cases below
     
     public static void main(String[] args) {
         Node<String> a = new Node<>("a");
@@ -24,10 +34,12 @@ class Node<T> {
         b.next = c;
         c.next = d;
 
-        // a -> b -> c -> d
+        
 
         System.out.println(Source.linkedListFind(a, "c"));
-        
-    }
+       
+        Node<Integer> node1 = new Node<>(100);
 
-  }
+        System.out.println(Source.linkedListFind(node1, 100)); 
+    }
+}
